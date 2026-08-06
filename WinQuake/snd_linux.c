4aadc2c8e6829e564053f00df93f3532d65e957d
EdgeOfAssembly <haxbox2000@gmail.com>
2026-08-06 03:21:39 +0300

    FEATURE v1 ALSA sound backend
    Replace OSS /dev/dsp mmap with ALSA write path in snd_linux.c:
    - Opens default (or \$AUDIODEV / \$ALSA_DEVICE)
    - 44.1/48 kHz S16 stereo ring buffer, power-of-two for mixer masks
    - SNDDMA_Submit via snd_pcm_writei; GetDMAPos via snd_pcm_delay
    - Link -lasound for quake.x11 and glquake
    
    Verify: ALSA: default 44100 Hz, 16 bit, stereo; holds /dev/snd/pcmC0D0p

--- WinQuake/snd_linux.c
+++ WinQuake/snd_linux.c
@@ -8,7 +8,7 @@ of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 See the GNU General Public License for more details.
 
@@ -17,253 +17,393 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <sys/shm.h>
-#include <sys/wait.h>
-#include <linux/soundcard.h>
+/*
+ * Linux sound: ALSA write backend (replaces OSS /dev/dsp mmap).
+ *
+ * Quake mixes into a power-of-two ring buffer (shm->buffer). We push
+ * newly painted frames to ALSA with snd_pcm_writei and report play
+ * position via snd_pcm_delay.
+ *
+ * Device: $AUDIODEV, else $ALSA_DEVICE, else "default".
+ * Overrides: -sndspeed, -sndbits, -sndmono / -sndstereo,
+ *            QUAKE_SOUND_SPEED / SAMPLEBITS / CHANNELS.
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <alsa/asoundlib.h>
+
 #include "quakedef.h"
 
-int audio_fd;
-int snd_inited;
+static snd_pcm_t	*pcm;
+static int		snd_inited;
+static int64_t		samples_written;	/* mono samples submitted to ALSA */
+static int		frame_bytes;		/* bytes per ALSA frame (all channels) */
 
-static int tryrates[] = { 11025, 22051, 44100, 8000 };
+static int tryrates[] = { 44100, 48000, 22050, 11025, 8000 };
 
-qboolean SNDDMA_Init(void)
+static const char *SND_DeviceName (void)
 {
+	const char	*d;
 
-	int rc;
-    int fmt;
-	int tmp;
-    int i;
-    char *s;
-	struct audio_buf_info info;
-	int caps;
+	d = getenv("AUDIODEV");
+	if (d && d[0])
+		return d;
+	d = getenv("ALSA_DEVICE");
+	if (d && d[0])
+		return d;
+	return "default";
+}
+
+static void SND_Recover (int err)
+{
+	if (err == -EPIPE)
+	{
+		/* underrun */
+		snd_pcm_prepare(pcm);
+	}
+	else if (err == -ESTRPIPE)
+	{
+		while ((err = snd_pcm_resume(pcm)) == -EAGAIN)
+			usleep(1000);
+		if (err < 0)
+			snd_pcm_prepare(pcm);
+	}
+}
+
+qboolean SNDDMA_Init (void)
+{
+	const char		*device;
+	int			err;
+	int			i;
+	char			*s;
+	unsigned int		rate;
+	unsigned int		channels;
+	snd_pcm_uframes_t	buffer_frames;
+	snd_pcm_uframes_t	period_frames;
+	snd_pcm_hw_params_t	*hw;
+	int			bits;
+	size_t			buf_bytes;
 
 	snd_inited = 0;
+	pcm = NULL;
+	samples_written = 0;
 
-// open /dev/dsp, confirm capability to mmap, and get size of dma buffer
-
-    audio_fd = open("/dev/dsp", O_RDWR);
-    if (audio_fd < 0)
+	device = SND_DeviceName();
+	err = snd_pcm_open(&pcm, device, SND_PCM_STREAM_PLAYBACK, SND_PCM_NONBLOCK);
+	if (err < 0)
 	{
-		perror("/dev/dsp");
-        Con_Printf("Could not open /dev/dsp\n");
-		return 0;
+		Con_Printf("ALSA: open '%s' failed: %s\n", device, snd_strerror(err));
+		return false;
 	}
 
-    rc = ioctl(audio_fd, SNDCTL_DSP_RESET, 0);
-    if (rc < 0)
+	/* ---- format preferences (same knobs as old OSS path) ---- */
+	bits = 16;
+	s = getenv("QUAKE_SOUND_SAMPLEBITS");
+	if (s)
+		bits = atoi(s);
+	else if ((i = COM_CheckParm("-sndbits")) != 0 && i + 1 < com_argc)
+		bits = atoi(com_argv[i + 1]);
+	if (bits != 8 && bits != 16)
+		bits = 16;
+
+	channels = 2;
+	s = getenv("QUAKE_SOUND_CHANNELS");
+	if (s)
+		channels = (unsigned)atoi(s);
+	else if (COM_CheckParm("-sndmono"))
+		channels = 1;
+	else if (COM_CheckParm("-sndstereo"))
+		channels = 2;
+
+	rate = 0;
+	s = getenv("QUAKE_SOUND_SPEED");
+	if (s)
+		rate = (unsigned)atoi(s);
+	else if ((i = COM_CheckParm("-sndspeed")) != 0 && i + 1 < com_argc)
+		rate = (unsigned)atoi(com_argv[i + 1]);
+
+	snd_pcm_hw_params_alloca(&hw);
+	err = snd_pcm_hw_params_any(pcm, hw);
+	if (err < 0)
 	{
-		perror("/dev/dsp");
-		Con_Printf("Could not reset /dev/dsp\n");
-		close(audio_fd);
-		return 0;
+		Con_Printf("ALSA: hw_params_any: %s\n", snd_strerror(err));
+		goto fail;
 	}
 
-	if (ioctl(audio_fd, SNDCTL_DSP_GETCAPS, &caps)==-1)
+	err = snd_pcm_hw_params_set_access(pcm, hw, SND_PCM_ACCESS_RW_INTERLEAVED);
+	if (err < 0)
 	{
-		perror("/dev/dsp");
-        Con_Printf("Sound driver too old\n");
-		close(audio_fd);
-		return 0;
+		Con_Printf("ALSA: set_access: %s\n", snd_strerror(err));
+		goto fail;
 	}
 
-	if (!(caps & DSP_CAP_TRIGGER) || !(caps & DSP_CAP_MMAP))
-	{
-		Con_Printf("Sorry but your soundcard can't do this\n");
-		close(audio_fd);
-		return 0;
-	}
-
-    if (ioctl(audio_fd, SNDCTL_DSP_GETOSPACE, &info)==-1)
-    {   
-        perror("GETOSPACE");
-		Con_Printf("Um, can't do GETOSPACE?\n");
-		close(audio_fd);
-		return 0;
-    }
-    
-	shm = &sn;
-    shm->splitbuffer = 0;
-
-// set sample bits & speed
-
-    s = getenv("QUAKE_SOUND_SAMPLEBITS");
-    if (s) shm->samplebits = atoi(s);
-	else if ((i = COM_CheckParm("-sndbits")) != 0)
-		shm->samplebits = atoi(com_argv[i+1]);
-	if (shm->samplebits != 16 && shm->samplebits != 8)
-    {
-        ioctl(audio_fd, SNDCTL_DSP_GETFMTS, &fmt);
-        if (fmt & AFMT_S16_LE) shm->samplebits = 16;
-        else if (fmt & AFMT_U8) shm->samplebits = 8;
-    }
-
-    s = getenv("QUAKE_SOUND_SPEED");
-    if (s) shm->speed = atoi(s);
-	else if ((i = COM_CheckParm("-sndspeed")) != 0)
-		shm->speed = atoi(com_argv[i+1]);
-    else
-    {
-        for (i=0 ; i<sizeof(tryrates)/4 ; i++)
-            if (!ioctl(audio_fd, SNDCTL_DSP_SPEED, &tryrates[i])) break;
-        shm->speed = tryrates[i];
-    }
-
-    s = getenv("QUAKE_SOUND_CHANNELS");
-    if (s) shm->channels = atoi(s);
-	else if ((i = COM_CheckParm("-sndmono")) != 0)
-		shm->channels = 1;
-	else if ((i = COM_CheckParm("-sndstereo")) != 0)
-		shm->channels = 2;
-    else shm->channels = 2;
-
-	shm->samples = info.fragstotal * info.fragsize / (shm->samplebits/8);
-	shm->submission_chunk = 1;
-
-// memory map the dma buffer
-
-	shm->buffer = (unsigned char *) mmap(NULL, info.fragstotal
-		* info.fragsize, PROT_WRITE, MAP_FILE|MAP_SHARED, audio_fd, 0);
-	if (!shm->buffer || shm->buffer == (unsigned char *)-1)
-	{
-		perror("/dev/dsp");
-		Con_Printf("Could not mmap /dev/dsp\n");
-		close(audio_fd);
-		return 0;
-	}
-
-	tmp = 0;
-	if (shm->channels == 2)
-		tmp = 1;
-    rc = ioctl(audio_fd, SNDCTL_DSP_STEREO, &tmp);
-    if (rc < 0)
-    {
-		perror("/dev/dsp");
-        Con_Printf("Could not set /dev/dsp to stereo=%d", shm->channels);
-		close(audio_fd);
-        return 0;
-    }
-	if (tmp)
-		shm->channels = 2;
+	if (bits == 8)
+		err = snd_pcm_hw_params_set_format(pcm, hw, SND_PCM_FORMAT_U8);
 	else
-		shm->channels = 1;
-
-    rc = ioctl(audio_fd, SNDCTL_DSP_SPEED, &shm->speed);
-    if (rc < 0)
-    {
-		perror("/dev/dsp");
-        Con_Printf("Could not set /dev/dsp speed to %d", shm->speed);
-		close(audio_fd);
-        return 0;
-    }
-
-    if (shm->samplebits == 16)
-    {
-        rc = AFMT_S16_LE;
-        rc = ioctl(audio_fd, SNDCTL_DSP_SETFMT, &rc);
-        if (rc < 0)
+		err = snd_pcm_hw_params_set_format(pcm, hw, SND_PCM_FORMAT_S16_LE);
+	if (err < 0)
+	{
+		/* fall back to S16 if U8 missing */
+		bits = 16;
+		err = snd_pcm_hw_params_set_format(pcm, hw, SND_PCM_FORMAT_S16_LE);
+		if (err < 0)
 		{
-			perror("/dev/dsp");
-			Con_Printf("Could not support 16-bit data.  Try 8-bit.\n");
-			close(audio_fd);
-			return 0;
+			Con_Printf("ALSA: set_format: %s\n", snd_strerror(err));
+			goto fail;
 		}
-    }
-    else if (shm->samplebits == 8)
-    {
-        rc = AFMT_U8;
-        rc = ioctl(audio_fd, SNDCTL_DSP_SETFMT, &rc);
-        if (rc < 0)
+	}
+
+	err = snd_pcm_hw_params_set_channels_near(pcm, hw, &channels);
+	if (err < 0)
+	{
+		Con_Printf("ALSA: set_channels: %s\n", snd_strerror(err));
+		goto fail;
+	}
+
+	if (rate == 0)
+	{
+		for (i = 0; i < (int)(sizeof(tryrates) / sizeof(tryrates[0])); i++)
 		{
-			perror("/dev/dsp");
-			Con_Printf("Could not support 8-bit data.\n");
-			close(audio_fd);
-			return 0;
+			rate = (unsigned)tryrates[i];
+			if (snd_pcm_hw_params_set_rate_near(pcm, hw, &rate, 0) >= 0)
+				break;
 		}
-    }
+	}
 	else
 	{
-		perror("/dev/dsp");
-		Con_Printf("%d-bit sound not supported.", shm->samplebits);
-		close(audio_fd);
-		return 0;
+		err = snd_pcm_hw_params_set_rate_near(pcm, hw, &rate, 0);
+		if (err < 0)
+		{
+			Con_Printf("ALSA: set_rate %u: %s\n", rate, snd_strerror(err));
+			goto fail;
+		}
 	}
 
-// toggle the trigger & start her up
-
-    tmp = 0;
-    rc  = ioctl(audio_fd, SNDCTL_DSP_SETTRIGGER, &tmp);
-	if (rc < 0)
+	/*
+	 * Ring size must be power-of-two in mono samples (snd_mix masks).
+	 * Aim ~0.25 s at the chosen rate, then round up to pow2 frames * ch.
+	 */
+	buffer_frames = (snd_pcm_uframes_t)(rate / 4);
+	if (buffer_frames < 1024)
+		buffer_frames = 1024;
+	/* next power of two */
 	{
-		perror("/dev/dsp");
-		Con_Printf("Could not toggle.\n");
-		close(audio_fd);
-		return 0;
+		snd_pcm_uframes_t	p = 1;
+		while (p < buffer_frames)
+			p <<= 1;
+		buffer_frames = p;
 	}
-    tmp = PCM_ENABLE_OUTPUT;
-    rc = ioctl(audio_fd, SNDCTL_DSP_SETTRIGGER, &tmp);
-	if (rc < 0)
+	period_frames = buffer_frames / 8;
+	if (period_frames < 64)
+		period_frames = 64;
+
+	err = snd_pcm_hw_params_set_period_size_near(pcm, hw, &period_frames, 0);
+	if (err < 0)
 	{
-		perror("/dev/dsp");
-		Con_Printf("Could not toggle.\n");
-		close(audio_fd);
-		return 0;
+		Con_Printf("ALSA: set_period_size: %s\n", snd_strerror(err));
+		goto fail;
+	}
+	err = snd_pcm_hw_params_set_buffer_size_near(pcm, hw, &buffer_frames);
+	if (err < 0)
+	{
+		Con_Printf("ALSA: set_buffer_size: %s\n", snd_strerror(err));
+		goto fail;
 	}
 
-	shm->samplepos = 0;
+	err = snd_pcm_hw_params(pcm, hw);
+	if (err < 0)
+	{
+		Con_Printf("ALSA: hw_params: %s\n", snd_strerror(err));
+		goto fail;
+	}
+
+	/* Read back negotiated geometry */
+	snd_pcm_hw_params_get_rate(hw, &rate, 0);
+	snd_pcm_hw_params_get_channels(hw, &channels);
+	snd_pcm_hw_params_get_buffer_size(hw, &buffer_frames);
+	snd_pcm_hw_params_get_period_size(hw, &period_frames, 0);
+
+	/*
+	 * Quake ring: mono sample count must be power of two.
+	 * Use 2x ALSA buffer (in frames) * channels, rounded to pow2.
+	 */
+	{
+		int	want = (int)buffer_frames * 2 * (int)channels;
+		int	p = 1;
+		while (p < want)
+			p <<= 1;
+		want = p;
+
+		shm = &sn;
+		shm->splitbuffer = 0;
+		shm->samplebits = bits;
+		shm->channels = (int)channels;
+		shm->speed = (int)rate;
+		shm->samples = want;
+		shm->samplepos = 0;
+		shm->submission_chunk = 1;
+
+		buf_bytes = (size_t)shm->samples * (size_t)(shm->samplebits / 8);
+		shm->buffer = (unsigned char *)calloc(1, buf_bytes);
+		if (!shm->buffer)
+		{
+			Con_Printf("ALSA: out of memory for %zu byte ring\n", buf_bytes);
+			goto fail;
+		}
+	}
+
+	frame_bytes = shm->channels * (shm->samplebits / 8);
+
+	err = snd_pcm_prepare(pcm);
+	if (err < 0)
+	{
+		Con_Printf("ALSA: prepare: %s\n", snd_strerror(err));
+		goto fail;
+	}
 
 	snd_inited = 1;
-	return 1;
+	Con_Printf("ALSA: %s  %d Hz, %d bit, %s, ring %d samples (period %lu frames)\n",
+		device, shm->speed, shm->samplebits,
+		shm->channels == 1 ? "mono" : "stereo",
+		shm->samples, (unsigned long)period_frames);
+	return true;
 
+fail:
+	if (pcm)
+	{
+		snd_pcm_close(pcm);
+		pcm = NULL;
+	}
+	if (shm && shm->buffer)
+	{
+		free(shm->buffer);
+		shm->buffer = NULL;
+	}
+	return false;
 }
 
-int SNDDMA_GetDMAPos(void)
+int SNDDMA_GetDMAPos (void)
 {
+	snd_pcm_sframes_t	delay = 0;
+	int64_t			played_frames;
+	int			pos;
 
-	struct count_info count;
-
-	if (!snd_inited) return 0;
-
-	if (ioctl(audio_fd, SNDCTL_DSP_GETOPTR, &count)==-1)
-	{
-		perror("/dev/dsp");
-		Con_Printf("Uh, sound dead.\n");
-		close(audio_fd);
-		snd_inited = 0;
+	if (!snd_inited || !pcm)
 		return 0;
-	}
-//	shm->samplepos = (count.bytes / (shm->samplebits / 8)) & (shm->samples-1);
-//	fprintf(stderr, "%d    \r", count.ptr);
-	shm->samplepos = count.ptr / (shm->samplebits / 8);
 
-	return shm->samplepos;
+	if (snd_pcm_delay(pcm, &delay) < 0)
+		delay = 0;
+	if (delay < 0)
+		delay = 0;
 
+	/* samples_written is mono samples; delay is frames still in device */
+	played_frames = (samples_written / shm->channels) - (int64_t)delay;
+	if (played_frames < 0)
+		played_frames = 0;
+
+	pos = (int)((played_frames * shm->channels) % (int64_t)shm->samples);
+	shm->samplepos = pos;
+	return pos;
 }
 
-void SNDDMA_Shutdown(void)
+void SNDDMA_Shutdown (void)
 {
-	if (snd_inited)
+	if (pcm)
 	{
-		close(audio_fd);
-		snd_inited = 0;
+		snd_pcm_drop(pcm);
+		snd_pcm_close(pcm);
+		pcm = NULL;
 	}
+	if (shm && shm->buffer)
+	{
+		free(shm->buffer);
+		shm->buffer = NULL;
+	}
+	snd_inited = 0;
+	samples_written = 0;
 }
 
 /*
 ==============
 SNDDMA_Submit
 
-Send sound to device if buffer isn't really the dma buffer
-===============
+Push painted samples that have not yet been written to ALSA.
+paintedtime is in sample pairs (stereo frames); see snd_dma.c.
+==============
 */
-void SNDDMA_Submit(void)
+void SNDDMA_Submit (void)
 {
-}
+	int		samplebits_bytes;
+	int		ring_bytes;
+	int64_t		painted_mono;
+	int64_t		to_write_mono;
+	int		frames;
+	int		err;
+	unsigned char	*src;
+	int		idx;
+	int		chunk_frames;
+	int		chunk_mono;
 
+	if (!snd_inited || !pcm || !shm || !shm->buffer)
+		return;
+
+	samplebits_bytes = shm->samplebits / 8;
+	ring_bytes = shm->samples * samplebits_bytes;
+
+	/* paintedtime = sample pairs; convert to mono sample count */
+	painted_mono = (int64_t)paintedtime * (int64_t)shm->channels;
+	to_write_mono = painted_mono - samples_written;
+	if (to_write_mono <= 0)
+		return;
+
+	/* Don't try to write more than one ring at once */
+	if (to_write_mono > shm->samples)
+		to_write_mono = shm->samples;
+
+	/* Align to whole frames */
+	to_write_mono -= to_write_mono % shm->channels;
+	if (to_write_mono <= 0)
+		return;
+
+	frames = (int)(to_write_mono / shm->channels);
+
+	while (frames > 0)
+	{
+		idx = (int)(samples_written % (int64_t)shm->samples);
+		/* Contiguous bytes until end of ring */
+		chunk_mono = shm->samples - idx;
+		if (chunk_mono > frames * shm->channels)
+			chunk_mono = frames * shm->channels;
+		chunk_mono -= chunk_mono % shm->channels;
+		if (chunk_mono <= 0)
+			break;
+		chunk_frames = chunk_mono / shm->channels;
+
+		src = shm->buffer + idx * samplebits_bytes;
+
+		err = (int)snd_pcm_writei(pcm, src, (snd_pcm_uframes_t)chunk_frames);
+		if (err == -EAGAIN)
+		{
+			/* device full — try again next frame */
+			break;
+		}
+		if (err < 0)
+		{
+			SND_Recover(err);
+			err = (int)snd_pcm_writei(pcm, src, (snd_pcm_uframes_t)chunk_frames);
+			if (err < 0)
+			{
+				Con_DPrintf("ALSA write: %s\n", snd_strerror(err));
+				break;
+			}
+		}
+
+		samples_written += (int64_t)err * shm->channels;
+		frames -= err;
+		(void)ring_bytes;
+	}
+}
